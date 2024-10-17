0f06f3bf4d278043d495aa1edef3e8bf2346d485
mkm203 <tapeyamiguelkelin@gmail.com>
2024-10-17 03:17:51 +0100

    Add files via upload

---
+++ PROGRAMMING ASSIGNMENT/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int name,age,gender;
+    printf("Enter your Name\n");
+    scanf("%d",&name);
+    printf("Enter your Age\n");
+    scanf("%d",&age);
+    printf("Enter your gender\n");
+    scanf("%d",&gender);
+    return 0;
+}
